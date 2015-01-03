085b15cc18c282d6a3a7b741352b1fc23ad14726
Maria Mercedes Martinez <pistachiopony@gmail.com>
2015-01-02 21:49:33 -0500

    fix import

--- creds.py
+++ creds.py
@@ -1,5 +1,5 @@
 login = {
     
     # 'mongodb_uri' : 'mongodb://martinezadmin:tintin33@ds039010.mongolab.com:39010/cyweapon'
-    'mongodb_uri' : 'mongodb : //heroku_app30624816:pnhp723e3270cfsoivro9g7aco@ds041160.mongolab.com:41160/heroku_app30624816'
+    'mongodb_uri' = 'mongodb : //heroku_app30624816:pnhp723e3270cfsoivro9g7aco@ds041160.mongolab.com:41160/heroku_app30624816'
 }
