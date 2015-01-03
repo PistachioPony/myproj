aa641ee156ec0bade904136fcb50c97766e4e68a
Maria Mercedes Martinez <pistachiopony@gmail.com>
2015-01-03 10:23:45 -0500

    fix route

--- app.py
+++ app.py
@@ -10,42 +10,43 @@ __author__ = 'mongolab'
 
 app = Flask(__name__)
 
-# app.config.update(
-#     DEBUG = True,
-# )
+app.config.update(
+    DEBUG = True,
+)
 
 MONGODB_URI = creds.login
 #-----------------------------------------
 # main
 #-----------------------------------------
 
+def main(args):
 
-client = pymongo.MongoClient(MONGODB_URI)
+    client = pymongo.MongoClient(MONGODB_URI)
 
-db = client.get_default_database()
+    db = client.get_default_database()
 
-perfumes = db['perfumes']
+    perfumes = db['perfumes']
+
+  
 
 
 #----------------------------------------
 # controllers
 #----------------------------------------
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
+    @app.route("/")
+    def index():
+        return render_template('index.html')
 #----------------------------------------
 # launch
 #----------------------------------------
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main(sys.argv[1:])
     
 
     # below is to run it locally
