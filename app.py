4401099b45e864d481eebe561b9930f9fa4b355d
Maria Mercedes Martinez <pistachiopony@gmail.com>
2015-01-02 21:02:10 -0500

    fix import

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import sys
 import pymongo
 from flask import Flask, render_template, send_from_directory
-import credentials.py
+from credentials import creds
 
 #----------------------------------------
 # initialization
@@ -15,7 +15,7 @@ app.config.update(
     DEBUG = True,
 )
 
-MONGODB_URI = credentials.login['mongodb_uri']
+MONGODB_URI = creds.login['mongodb_uri']
 
 #-----------------------------------------
 # main
