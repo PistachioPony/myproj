fba611e4b3313217800091cf307bbe927a550b2b
Maria Mercedes Martinez <pistachiopony@gmail.com>
2015-01-02 22:00:21 -0500

    fix import

--- creds.py
+++ creds.py
@@ -1,4 +1,4 @@
-login = 'mongodb : //heroku_app30624816:pnhp723e3270cfsoivro9g7aco@ds041160.mongolab.com:41160/heroku_app30624816'
+login = 'mongodb://heroku_app30624816:pnhp723e3270cfsoivro9g7aco@ds041160.mongolab.com:41160/heroku_app30624816'
 
 
 # 'mongodb_uri' : 'mongodb://martinezadmin:tintin33@ds039010.mongolab.com:39010/cyweapon'
