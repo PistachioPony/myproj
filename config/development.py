ca1537bba2f142dba139df8ef7cce4d8dc125a4d
Maria Mercedes Martinez <pistachiopony@gmail.com>
2015-02-01 18:33:11 -0500

    revert to state before hierarchy folders and stormpath add

---
+++ config/development.py
@@ -0,0 +1,7 @@
+login = "mongodb://heroku_app30624816:pnhp723e3270cfsoivro9g7aco@ds041160.mongolab.com:41160/heroku_app30624816"
+
+# 'mongodb_uri' : 'mongodb://martinezadmin:tintin33@ds039010.mongolab.com:39010/cyweapon'
+app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+STORMPATH_API_KEY_ID = "LSSGXN41D9CVYLX825PD2BPKX"
+STORMPATH_API_KEY_SECRET = "2hQhakpI2+YpapLTXyt9WRwxMysc7pGSZkMhFsYe66k"
+STORMPATH_URL = "https://api.stormpath.com/v1/applications/1ORk9GEJNH4qWYvedFIeIZ"
